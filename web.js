f6f4448d3823b7fa64a224b067a052cf41e0d5b8
Vishal Joshi <vmjoshi@umich.edu>
2013-07-12 14:42:42 +0000

    Updatd web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var filename = "index.html";
+  var data = fs.readFileSync(filename);
+  var str = data.toString 
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
